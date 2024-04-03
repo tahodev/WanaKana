58bf864f2607c5a86f862b03f73222db75641e26
Duncan Bay <duncan.bay@gmail.com>
2024-04-03 08:42:58 +0700

    fix: config keys in tokenize should be optional for typescript types

--- src/tokenize.js
+++ src/tokenize.js
@@ -68,8 +68,8 @@ export function getType(input, compact = false) {
  * If `{ compact: true }` then many same-language tokens are combined (spaces + text, kanji + kana, numeral + punctuation).
  * If `{ detailed: true }` then return array will contain `{ type, value }` instead of `'value'`
  * @param  {String} input text
- * @param  {{compact: Boolean | undefined, detailed: Boolean | undefined}} [options={ compact: false, detailed: false}] options to modify output style
- * @return {(String[]|Array.<{type: String, value: String}>)} text split into tokens containing values, or detailed object
+ * @param  {{compact?: Boolean, detailed?: Boolean}} [options={ compact: false, detailed: false}] options to modify output style
+ * @return {(String[]|{type: String, value: String}[])} text split into tokens containing values, or detailed object
  * @example
  * tokenize('ふふフフ')
  * // ['ふふ', 'フフ']
